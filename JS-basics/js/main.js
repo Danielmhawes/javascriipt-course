30e2d56597ad715c5e15a6cdcb12cfa71102bee7
Danielmhawes <danielmhawes002@gmail.com>
2020-04-23 15:21:45 +0200

    finished the basic fundementals

--- JS-basics/js/main.js
+++ JS-basics/js/main.js
@@ -640,10 +640,85 @@ GOOD LUCK 😀
 var john = {
   fullname: 'John Smith',
   bills: [125 , 48, 268, 180, 42],
-  calcTips: function()[]
+  calcTips: function(){
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++){
+      // determine percentage based on tipping rules
+      var percentage;
+      var bill = this.bills[i]
+
+      if (bill < 50){
+        percentage = 0.2
+      }else if (bill >= 50 && bill < 200 ){
+        percentage = 0.15
+      }else {
+        percentage = 0.1;
+      }
+      
+      // add results to the corresponding arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+    }
+  }
 }
 
 
+
+// 77, $375, $110, and $45.
+var mark = {
+  fullname: 'Mark Miller',
+  bills: [77 , 375, 110, 45],
+  calcTips: function(){
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++){
+      // determine percentage based on tipping rules
+      var percentage;
+      var bill = this.bills[i]
+
+      if (bill < 100){
+        percentage = 0.2
+      }else if (bill >= 100 && bill < 300 ){
+        percentage = 0.1;
+      }else {
+        percentage = 0.25;
+      }
+      
+      // add results to the corresponding arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+    }
+  }
+}
+
+
+function calcAverage(tips){ 
+  var sum = 0;
+  for (var i = 0; i < tips.length; i++){
+    sum = sum + tips[i];
+  }
+  return sum/tips.length
+}
+//[2, 6, 4] -> 0 / 2 / 8
+
+
+//do the calculations
+john.calcTips();
+mark.calcTips();
+
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+if (john.average > mark.average){
+  console.log(john.fullname + '\'s family pays higher tips, with an average of $' + john.average)
+} else if (mark.average > john.average){
+  console.log(mark.fullname + '\'s family pays higher tips, with an average of $' + mark.average)
+}
 
 
 
